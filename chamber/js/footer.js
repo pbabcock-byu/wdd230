e478787c3501e5b6ad2c217e23333705dcf096cc
pbabcock-byu <peterb@saefl.com>
2022-05-13 21:01:34 -0700

    update

---
+++ chamber/js/footer.js
@@ -0,0 +1,9 @@
+const format = {year: "numeric"}
+const currentyear = new Date().toLocaleDateString("en-US",format);
+
+/* This adds the copyright symbol and will update to the current year */
+document.getElementById("currentyear").innerHTML= `&copy; ${currentyear} .:|:. Peter Babcock .:|:.  California`;
+
+/*This adds the date and time when the document was last modified*/
+document.getElementById("lastupdate").innerHTML = document.lastModified;
+
