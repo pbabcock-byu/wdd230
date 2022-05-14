ff05945242e499db3c53bf931391d0cb7e8d1740
pbabcock-byu <peterb@saefl.com>
2022-05-14 00:57:07 -0700

    updates

--- chamber/js/footer.js
+++ chamber/js/footer.js
@@ -2,8 +2,16 @@ const format = {year: "numeric"}
 const currentyear = new Date().toLocaleDateString("en-US",format);
 
 /* This adds the copyright symbol and will update to the current year */
-document.getElementById("currentyear").innerHTML= `&copy; ${currentyear} .:|:. Peter Babcock .:|:.  California`;
+document.getElementById("currentyear").innerHTML= `&copy; ${currentyear} Kempton Park Chamber .:|:. Peter Babcock .:|:.  WDD 230  .:|:.  Last Updated: ${document.lastModified}`;
 
-/*This adds the date and time when the document was last modified*/
-document.getElementById("lastupdate").innerHTML = document.lastModified;
+/*This adds the date and time when the document was last modified
+document.getElementById("lastupdate").innerHTML = document.lastModified;*/
 
+/* for menu bar */
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
