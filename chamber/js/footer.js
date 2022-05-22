bb1325d1193fe0f2734fb825be620e74563c9567
pbabcock-byu <peterb@saefl.com>
2022-05-21 22:48:13 -0700

    week 5 updates

--- chamber/js/footer.js
+++ chamber/js/footer.js
@@ -20,3 +20,13 @@ const x = document.getElementById("hamburgerbtn");
 
 x.onclick = toggleMenu; 
 
+/*banner at the top of webpage */
+const today = now.getDay();
+const chambermeeting = document.getElementById("chambermeeting");
+if(today === 1 || today === 2) {
+    chambermeeting.style.display = "block";
+}
+else {
+    chambermeeting.style.display = "none";
+}
+
