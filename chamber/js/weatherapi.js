a2fedf181503e938a1b2d7c74f2e05b26ced244e
pbabcock-byu <peterb@saefl.com>
2022-06-22 12:46:43 -0700

    update : cleanups

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -3,19 +3,25 @@ const currentTemp = document.querySelector('#temperature');
 const windspeed = document.querySelector('#windspeed');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const Humidity= document.querySelector("#Humidity");
+const Hightemperature = document.querySelector("#Hightemperature");
+const Lowtemperature = document.querySelector("#Lowtemperature");
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-33.9258&lon=18.4232&appid=c19f1da1cd37c68484ca5bb501e2b781';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-33.9258&lon=18.4232&units=metric&appid=c19f1da1cd37c68484ca5bb501e2b781';
 
-
+//https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
 
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
     console.log(data); // this is temporary for development only
-    currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
+
+
+    currentTemp.innerHTML = `<strong>${(data.main.temp * 9/5 + 32).toFixed(2)}</strong>`;
     windspeed.innerHTML = `<strong>${data.wind.speed.toFixed(2)}</strong>`;
-
-
+    Humidity.innerHTML = `<strong>${data.main.humidity.toFixed(2)}</strong>`;
+    Hightemperature.innerHTML = `<strong>${data.main.temp_max.toFixed(2)}</strong>`;
+    Lowtemperature.innerHTML = `<strong>${data.main.temp_min.toFixed(2)}</strong>`;
     
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     const desc = data.weather[0].description;
