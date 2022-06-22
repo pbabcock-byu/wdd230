5c5a3542a42e59c38b96c0cd1169719bddc029ee
pbabcock-byu <peterb@saefl.com>
2022-06-22 14:33:15 -0700

    updated weather api

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -1,27 +1,41 @@
 // select HTML elements to edit
 const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
+const weatherIcon = document.querySelector('#weathericon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=c19f1da1cd37c68484ca5bb501e2b781';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=c19f1da1cd37c68484ca5bb501e2b781';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+
+apiFetch();
 
 
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data); // testing data
-    currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
-
-
-    
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    const desc = data.weather[0].description;
-    
-    weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
-    
-  });
-
-
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = (weatherData.weather[0].description).toUpperCase();
   
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+
+}
+
+
+
+
